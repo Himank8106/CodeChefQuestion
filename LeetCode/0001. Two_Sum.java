c0fdd452a39dc35ff395c05cc8922b04f573d660
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2023-08-26 09:03:15 +0530

    Create 0001. Two_Sum.java

---
+++ LeetCode/0001. Two_Sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {       
+        int c = 1;
+        while(true){
+            for(int i = 0; i < nums.length-c; i++){
+                if(nums[i] + nums[i+c] == target){
+                    return new int[]{i, i+c};
+                }
+            }
+        c++;
+        }
+    }
+}
+
+
+/*
+int[] ans = new int[2];
+for(int i=0; i<nums.length; i++){
+    for(int j=i+1; j<nums.length; j++){
+        if(nums[i]+nums[j]==target){
+            ans[0]=i;
+            ans[1]=j;
+        }
+    }
+} 
+return ans;
+*/
