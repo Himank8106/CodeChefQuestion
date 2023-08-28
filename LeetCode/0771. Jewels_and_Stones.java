62b2edaada3a6c12428ee84908d39efc66c2a5df
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2023-08-28 09:55:19 +0530

    Create 0771. Jewels_and_Stones.java

---
+++ LeetCode/0771. Jewels_and_Stones.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int res=0;
+        for(int i=0; i<jewels.length(); i++) {
+            char ch1=jewels.charAt(i);
+            for(int j=0; j<stones.length(); j++) {
+                char ch2=stones.charAt(j);
+                if(ch1==ch2)     
+                    res++;
+            }
+        }
+        return res;
+    }
+}
+
+
+/*
+int count = 0;
+for(int i=0; i<stones.length(); i++){
+    for(int j=0; j<jewels.length(); j++){
+        if(jewels.charAt(j)==stones.charAt(i)){
+            count++;
+        }
+    }
+}
+return count;
+*/
