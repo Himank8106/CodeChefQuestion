f18d5affb70780260da00d9abff2dd3d28417720
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2023-08-28 10:23:06 +0530

    Create 2413. Smallest_Even_Multiple.java

---
+++ LeetCode/2413. Smallest_Even_Multiple.java
@@ -0,0 +1,8 @@
+/* Link - https://leetcode.com/problems/smallest-even-multiple/ */
+
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        if(n%2==0) return n;
+        return n*2;
+    }
+}
