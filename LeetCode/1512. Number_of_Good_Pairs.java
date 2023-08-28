0559ea085d578a7fa49286f221ed475c734758af
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2023-08-28 10:11:37 +0530

    Create 1512. Number_of_Good_Pairs.java

---
+++ LeetCode/1512. Number_of_Good_Pairs.java
@@ -0,0 +1,28 @@
+/* Link - https://leetcode.com/problems/number-of-good-pairs/ */
+
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+    int count=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(i<j&&nums[i]==nums[j]){
+                       count++;
+                }
+            }
+        }
+        return count;
+    }
+}
+
+
+/*
+        int count = 0;
+        for(int i=0; i<nums.length-1; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
+            }
+        }
+        return count;
+*/
