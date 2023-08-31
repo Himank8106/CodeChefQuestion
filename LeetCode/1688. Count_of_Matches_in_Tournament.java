d23a64330f601a30f1efc7f94cd518ef7e101d76
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2023-08-31 23:30:07 +0530

    Create 1688. Count_of_Matches_in_Tournament.java

---
+++ LeetCode/1688. Count_of_Matches_in_Tournament.java
@@ -0,0 +1,16 @@
+/* Link - https://leetcode.com/problems/count-of-matches-in-tournament/ */
+
+class Solution {
+    public int numberOfMatches(int n) {
+        int count = 0;
+        while(n>1){
+            int x = n%2;
+            if((n - x)%2 == 0){
+                n = n/2;
+                count+=n;
+            }
+            n+=x;
+        }
+        return count;
+    }
+}
