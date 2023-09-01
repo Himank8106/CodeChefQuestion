4273bf7dd7dca726567521cf5471270f55722fcf
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2023-09-01 22:40:48 +0530

    Create 0242. Valid_Anagram.java

---
+++ LeetCode/0242. Valid_Anagram.java
@@ -0,0 +1,31 @@
+/* Link - https://leetcode.com/problems/valid-anagram/ */
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        
+        int[] freq = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
+        }
+        
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
+
+/* 
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        return Arrays.equals(sChars, tChars);
+*/
