1cd4e0783964162497656dc62607f892257d594a
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2023-09-03 21:48:49 +0530

    Create CHEFSTR1.java

---
+++ CodeChef/CHEFSTR1.java
@@ -0,0 +1,31 @@
+/* Link - https://www.codechef.com/problems/CHEFSTR1 */
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner scan = new Scanner(System.in);
+		byte t = scan.nextByte();
+		while(t-->0)
+		{
+		    int n = scan.nextInt(),a=0,b=0;
+		    long ans=0;
+		    a = scan.nextInt();
+		    for(int i=1; i<n; i++)
+		    {
+		        b = scan.nextInt();
+		        ans = ans + Math.abs(a-b) - 1;
+		        a = b;
+		    }
+		    System.out.println(ans);
+		}
+	}
+}
