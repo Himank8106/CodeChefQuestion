165566f94847ecfd87321a2568dc5401fbcbce2d
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2023-09-05 20:13:51 +0530

    Create FRIMEET.java

---
+++ CodeChef/FRIMEET.java
@@ -0,0 +1,28 @@
+/* Link - https://www.codechef.com/problems/FRIMEET */ 
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+	    int t = sc.nextInt();
+	    for(int i=0; i<t; i++){
+	        int x1 = sc.nextInt();
+	        int x2 = sc.nextInt();
+	        if(x1==x2 || x1>x2){
+	            System.out.println("YES");
+	        }
+	        else{
+	            System.out.println("NO");
+	        }
+	    }
+	}
+}
