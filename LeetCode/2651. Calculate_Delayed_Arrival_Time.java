069b7f3bfdc4a717f54627e73cfca173400897ed
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2023-09-08 22:50:05 +0530

    Create 2651. Calculate_Delayed_Arrival_Time.java

---
+++ LeetCode/2651. Calculate_Delayed_Arrival_Time.java
@@ -0,0 +1,7 @@
+/* Link - https://leetcode.com/problems/calculate-delayed-arrival-time/ */
+
+class Solution {
+    public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+       return (arrivalTime+delayedTime)%24;
+    }
+}
