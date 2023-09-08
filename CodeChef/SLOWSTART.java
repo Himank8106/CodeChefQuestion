10b3558ec5951222fabdfb4abce61acc279e3804
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2023-09-08 22:40:03 +0530

    Create SLOWSTART.java

---
+++ CodeChef/SLOWSTART.java
@@ -0,0 +1,31 @@
+/* Link - https://www.codechef.com/problems/SLOWSTART */
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int x = sc.nextInt() , h = sc.nextInt(), i = 0, count = 0;
+		    while(h>0 && i<5){
+		        h -= (x/2);
+		        count++;
+		        i++;
+		    }
+		    while(h>0){
+		        h -= x;
+		        count++;
+		    }
+		    System.out.println(count);
+		}
+	}
+}
