79fc80176950503d4affeb4537ac3a4d19712957
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2023-09-14 22:39:08 +0530

    Create 2703. Return_Length_of_Arguments_Passed.js

---
+++ LeetCode/2703. Return_Length_of_Arguments_Passed.js
@@ -0,0 +1,12 @@
+/* Link - https://leetcode.com/problems/return-length-of-arguments-passed/ */
+
+/**
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
