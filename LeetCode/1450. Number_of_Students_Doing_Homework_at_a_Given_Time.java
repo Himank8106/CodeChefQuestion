698ce491f4098078960fb36a00244b063b440a24
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2023-09-26 20:56:01 +0530

    Create 1450. Number_of_Students_Doing_Homework_at_a_Given_Time.java

---
+++ LeetCode/1450. Number_of_Students_Doing_Homework_at_a_Given_Time.java
@@ -0,0 +1,13 @@
+/* Link - https://leetcode.com/problems/number-of-students-doing-homework-at-a-given-time/ */
+
+class Solution {
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int count = 0;
+        for(int i=0; i<startTime.length; i++){
+            if(queryTime >= startTime[i] && queryTime<=endTime[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
