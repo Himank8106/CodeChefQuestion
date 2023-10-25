9374b6671ac6e3ee3d55f70f951f92db71e5af76
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2023-10-25 12:33:16 +0530

    Create 1356. Sort_Integers_by_The_Number_of_1_Bits.java

---
+++ LeetCode/1356. Sort_Integers_by_The_Number_of_1_Bits.java
@@ -0,0 +1,14 @@
+/* Link - https://leetcode.com/problems/sort-integers-by-the-number-of-1-bits/ */
+
+class Solution {
+    public int[] sortByBits(int[] arr) {
+        for(int i=0; i<arr.length; i++){
+            arr[i] += Integer.bitCount(arr[i])*10001;
+        }
+        Arrays.sort(arr);
+        for(int i=0; i<arr.length; i++){
+            arr[i] = arr[i]%10001;
+        }
+        return arr;
+    }
+}
