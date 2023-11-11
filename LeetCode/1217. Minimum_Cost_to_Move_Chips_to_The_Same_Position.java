f27b48fee019289312e87300c2249ae912fb3952
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2023-11-11 12:59:57 +0530

    Create 1217. Minimum_Cost_to_Move_Chips_to_The_Same_Position.java

---
+++ LeetCode/1217. Minimum_Cost_to_Move_Chips_to_The_Same_Position.java
@@ -0,0 +1,12 @@
+/* Link - https://leetcode.com/problems/minimum-cost-to-move-chips-to-the-same-position/ */
+
+class Solution {
+    public int minCostToMoveChips(int[] position) {
+        int even=0, odd=0;
+        for(int i: position){
+            if(i%2==0) even++;
+            else odd++;
+        }
+        return Math.min(even,odd);
+    }
+}
