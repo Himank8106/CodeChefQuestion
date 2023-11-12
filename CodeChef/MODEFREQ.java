43711ece75e71012144b7de24e23ac27f637bf1d
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2023-11-12 15:52:39 +0530

    Create MODEFREQ.java

---
+++ CodeChef/MODEFREQ.java
@@ -0,0 +1,41 @@
+/* Link - https://www.codechef.com/problems/MODEFREQ */
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+	    int t = sc.nextInt();
+	    while(t-- > 0){
+	        int n = sc.nextInt();
+	        int [] frequencies = new int [11];
+	        int [] frequencies2 = new int [10001];
+	        for(int i = 0; i<n; i++){
+	            frequencies[sc.nextInt()] += 1;
+	            
+	        }
+	        for(int i = 1; i<11; i++){
+	            frequencies2[frequencies[i]] += 1;
+	        }
+	        int maxFreq = -1;
+	        for(int i = 1; i<10001; i++){
+	            maxFreq = Math.max(maxFreq, frequencies2[i]);
+	        }
+	        for(int i = 1; i<10001; i++){
+	            if(frequencies2[i] == maxFreq) {
+	                System.out.println(i);
+	                break;
+	           
+	            }
+	        }
+	    }
+	}
+}
