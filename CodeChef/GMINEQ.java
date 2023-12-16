6a5141920848b9c242b27617b95e99713229f3ca
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2023-12-16 21:07:02 +0530

    Create GMINEQ.java

---
+++ CodeChef/GMINEQ.java
@@ -0,0 +1,31 @@
+/* Link - https://www.codechef.com/problems/GMINEQ */
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes hereScanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while( t-- > 0 ){
+		    int n = sc.nextInt();
+		    int pos = 0, neg = 0, a;
+		    for(int i = 0; i < n; i++){
+		        a = sc.nextInt();
+		        if(a > 0) pos++;
+		        else neg++;
+		    }
+		    a = Math.abs(pos - neg);
+		    if(a == 0 || a == 1) System.out.println("Yes");
+		    else if(a == 2 && pos % 2 == 0) System.out.println("Yes");
+		    else System.out.println("No");
+		}
+	}
+}
