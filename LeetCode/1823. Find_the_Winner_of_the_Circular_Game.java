8f5eaf9fe517420303f353d52f185d770b08636a
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-01-06 19:26:20 +0530

    Create 1823. Find_the_Winner_of_the_Circular_Game.java

---
+++ LeetCode/1823. Find_the_Winner_of_the_Circular_Game.java
@@ -0,0 +1,15 @@
+/* Link - https://leetcode.com/problems/find-the-winner-of-the-circular-game/ */
+
+class Solution {
+    public int findTheWinner(int n, int k) {
+        return help(n,k)+1;
+    }
+    int help(int n,int k){
+       if(n==1) 
+     return 0;
+    
+        int x=help(n-1,k);
+        int y=(x+k)%n;
+        return y;
+    }
+}
