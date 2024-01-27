3e46cbef890f954d626c9889f4010fd390f660af
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-01-27 19:35:24 +0530

    Create At_the_Gates.java

---
+++ CodeChef/At_the_Gates.java
@@ -0,0 +1,46 @@
+/* Link - https://www.codechef.com/problems/POPGATES */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+      Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0) {
+		    int n = sc.nextInt();
+		    int k = sc.nextInt();
+		    
+		    char[] coin = new char[n];
+		    
+		    for(int i=0; i<n; i++) 
+		    coin[i] = sc.next().charAt(0);
+		    
+		    int ans = 0;
+		    int cunt = 0;
+		    
+		    for(int i=n-1; i>=(n-k); i--) {
+		        if(coin[i] == 'H') {
+		            change(coin, n);
+		        }
+		    }
+		    
+		    for(int i=0; i<n-k; i++) {
+		        if(coin[i] == 'H') ans++;
+		    }
+		    
+		    System.out.println(ans);
+		}
+	}
+	
+	public static void change(char[] coin, int n) {
+	    for(int i=0; i<n; i++) {
+	        if(coin[i] == 'H') coin[i] = 'T';
+	        else coin[i] = 'H';
+	    }
+	}
+}
