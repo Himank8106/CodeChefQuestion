70695f202f8024bf075227530d3de4b68a43341b
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-01-28 23:34:57 +0530

    Create Plus_Multiply.java

---
+++ CodeChef/Plus_Multiply.java
@@ -0,0 +1,32 @@
+/* Link - https://www.codechef.com/problems/PLMU */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+              int n = sc.nextInt();
+		    int count2=0;
+		    int count0=0;
+		    int[] arr = new int[n];
+		    for(int i=0;i<n;i++){
+		        arr[i]=sc.nextInt();
+		        if(arr[i]==2){
+		            count2++;
+		        }
+		        if(arr[i]==0){
+		            count0++;
+		        }
+		    }
+		    int ans = ((count2*(count2-1))/2+(count0*(count0-1))/2);
+		    System.out.println(ans);
+		}
+	}
+}
