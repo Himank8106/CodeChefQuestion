f8de8790ac6fe3b8abfa4947bb838b7e09f97823
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-02-04 17:01:57 +0530

    Create Wave_Array.java

---
+++ InterviewBit/Wave_Array.java
@@ -0,0 +1,11 @@
+/* Link - https://www.interviewbit.com/problems/wave-array/ */
+
+public class Solution {
+    public ArrayList<Integer> wave(ArrayList<Integer> A) {
+        Collections.sort(A);
+        for(int i=0; i<A.size()-1; i+=2){
+            Collections.swap(A, i, i+1);
+        }
+        return A;
+    }
+}
