6d01fac5d96e2aefb76a86d5565133a7fbade8af
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-02-04 15:54:50 +0530

    Create FANCY.java

---
+++ CodeChef/FANCY.java
@@ -0,0 +1,28 @@
+/* Link - https://www.codechef.com/problems/FANCY */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+        while(t-->0){
+            String[] str = sc.nextLine().split(" ");
+            boolean b = false;
+            for(String s: str){
+                if(s.equals("not")){
+                    b = true;
+                    break;
+                }
+            }
+            if(b) System.out.println("Real Fancy");
+            else System.out.println("regularly fancy");
+        }
+	}
+}
