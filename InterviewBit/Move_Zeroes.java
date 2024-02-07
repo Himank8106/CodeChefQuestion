52ae34a80423a467862d5d9fa5cfdc3ff05a5034
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-02-07 23:01:03 +0530

    Create Move_Zeroes.java

---
+++ InterviewBit/Move_Zeroes.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public ArrayList<Integer> solve(ArrayList<Integer> A) {
+        int count = 0;
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i=0; i<A.size(); i++){
+            if(A.get(i)>0){
+                ans.add(A.get(i));
+            }
+            else{
+                count++;
+            }
+        }
+        for(int i=ans.size(); i<A.size(); i++){
+            ans.add(0);
+        }
+        return ans; 
+    }
+}
