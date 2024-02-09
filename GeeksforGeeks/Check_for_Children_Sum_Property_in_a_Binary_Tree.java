7235e529f03d4c7234cf6eb1cb6cacba6408c14a
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-02-09 11:26:48 +0530

    Create Check_for_Children_Sum_Property_in_a_Binary_Tree.java

---
+++ GeeksforGeeks/Check_for_Children_Sum_Property_in_a_Binary_Tree.java
@@ -0,0 +1,15 @@
+/* Link - https://www.geeksforgeeks.org/problems/children-sum-parent/1 */
+
+class Solution
+{
+    public static int isSumProperty(Node root)
+    {
+        if(root==null) return 1;
+        if(root.left==null & root.right==null) return 1;
+        int sum=0;
+        if(root.left!=null)sum+=root.left.data;
+        if(root.right!=null)sum+=root.right.data;
+        if(root.data==sum) return (isSumProperty(root.left)&isSumProperty(root.right));
+        return 0;
+    }
+}
