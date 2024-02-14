129aa200302bc6af5353384e023bff18fa7e16fe
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-02-14 10:46:47 +0530

    Create CHEFFAV.java

---
+++ CodeChef/CHEFFAV.java
@@ -0,0 +1,26 @@
+/* Link - https://www.codechef.com/problems/CHEFFAV */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            String s = sc.next();
+            int a = s.indexOf("code"), b = s.indexOf("chef");
+            if(a>b){
+                System.out.println("WA");
+            }
+            else{
+                System.out.println("AC");
+            }
+        }
+	}
+}
