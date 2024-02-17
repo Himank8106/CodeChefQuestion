3bfea3aa06114aa2a988db935e7e8b6306a5990c
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-02-17 10:47:29 +0530

    Create Does_array_represent_Heap.java

---
+++ GeeksforGeeks/Does_array_represent_Heap.java
@@ -0,0 +1,15 @@
+/* Link - https://www.geeksforgeeks.org/problems/does-array-represent-heap4345/1 */
+
+class Solution {
+    
+    public boolean countSub(long arr[], long n)
+    {
+        // Your code goes here
+         for(int i=(int)n-1;i>0;i--){
+            int p=(i-1)/2;
+            if(arr[i]>arr[p])
+                return false;
+        }
+        return true;
+    }
+}
