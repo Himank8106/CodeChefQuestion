f6de9634dca41c5663c88ddfc5ac29eb99d40355
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-02-18 12:31:31 +0530

    Create FALSNUM.java

---
+++ CodeChef/FALSNUM.java
@@ -0,0 +1,28 @@
+/* Link - https://www.codechef.com/problems/FALSNUM */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    String s=sc.next();
+		    if(s.charAt(0)=='1')
+		    {
+		        s="10"+s.substring(1);
+		    }
+		    else
+		    {
+		        s='1'+s;
+		    }
+		    System.out.println(s);
+		}
+	}
+}
