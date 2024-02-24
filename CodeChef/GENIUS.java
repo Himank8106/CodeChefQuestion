124ec5b062d25392cc593e81553c036a57ca5b12
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-02-24 12:36:58 +0530

    Create GENIUS.java

---
+++ CodeChef/GENIUS.java
@@ -0,0 +1,27 @@
+/* Link - https://www.codechef.com/problems/GENIUS */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+        int totalQuestions = sc.nextInt();
+		int score = sc.nextInt();
+		int stats = (score+2) / 3;
+		if(stats * 3 - score + stats > totalQuestions) {
+		    System.out.println("NO");
+		} else {
+		    System.out.println("YES");
+		    System.out.println(stats + " " + (stats * 3 - score) + " " + (totalQuestions - stats * 4 + score));
+		    }
+            
+        }
+	}
+}
