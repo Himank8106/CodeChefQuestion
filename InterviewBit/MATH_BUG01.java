2b3bb941d26c3b6981ef5bf3b33fa569d853a208
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-02-25 11:27:28 +0530

    Create MATH_BUG01.java

---
+++ InterviewBit/MATH_BUG01.java
@@ -0,0 +1,12 @@
+/* Link - https://www.interviewbit.com/problems/mathbug01/ */
+
+public class Solution {
+	public int isPrime(int A) {
+		int upperLimit = (int)(Math.sqrt(A));
+		if(A == 1) return 0;
+		for (int i = 2; i <= upperLimit; i++) {
+			if (A % i == 0) return 0;
+		}
+		return 1;
+	}
+}
