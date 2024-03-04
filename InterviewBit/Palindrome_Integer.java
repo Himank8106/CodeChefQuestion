12c2ae016b633a998d98a9b40d2f751d3ce26c49
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-03-04 11:56:24 +0530

    Create Palindrome_Integer.java

---
+++ InterviewBit/Palindrome_Integer.java
@@ -0,0 +1,14 @@
+/* Link - https://www.interviewbit.com/problems/palindrome-integer/ */
+
+public class Solution {
+    public int isPalindrome(int A) {
+        int temp = A;
+        int ans = 0;
+        while(temp > 0){
+            ans = temp%10 + ans*10;
+            temp /= 10;
+        }
+        if( ans == A) return 1;
+        return 0;
+    }
+}
