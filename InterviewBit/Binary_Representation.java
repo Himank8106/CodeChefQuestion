104a1a08413654e689966604764049e5a1904636
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-03-09 20:50:43 +0530

    Create Binary_Representation.java

---
+++ InterviewBit/Binary_Representation.java
@@ -0,0 +1,7 @@
+/* Link - https://www.interviewbit.com/problems/binary-representation/ */
+
+public class Solution {
+    public String findDigitsInBinary(int A) {
+        return Integer.toBinaryString(A);
+    }
+}
