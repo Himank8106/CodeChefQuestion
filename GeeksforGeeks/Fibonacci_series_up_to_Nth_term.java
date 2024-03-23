df4f4814c168ef6fc07f6ff03b149072d7e1bcbe
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-03-23 17:31:12 +0530

    Create Fibonacci_series_up_to_Nth_term.java

---
+++ GeeksforGeeks/Fibonacci_series_up_to_Nth_term.java
@@ -0,0 +1,19 @@
+/* Link - https://www.geeksforgeeks.org/problems/fibonacci-series-up-to-nth-term/1 */
+
+class Solution {
+
+    int[] Series(int n) {
+        // code here
+        int[] ar = new int[n+1];
+        ar[0] = 0;
+        ar[1] = 1;
+        int i=2;
+        int mod = (int)(1e9 + 7);
+        while(i<n+1)
+        {
+            ar[i] = (ar[i-1] + ar[i-2]) % mod;
+            i++;
+        }
+        return ar;
+    }
+}
