1a9b4dae2d3e885e8a211e8801e59c40959f2874
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-03-31 10:00:46 +0530

    Create 2444. Count_Subarrays_With_Fixed_Bounds.java

---
+++ LeetCode/2444. Count_Subarrays_With_Fixed_Bounds.java
@@ -0,0 +1,47 @@
+/* Link - https://leetcode.com/problems/count-subarrays-with-fixed-bounds/ */
+
+class Solution {
+    public long countSubarrays(int[] nums, int minK, int maxK) {
+        int minI = -1, maxI = -1, left = -1, right = 0;
+        long count = 0;
+        while(right < nums.length){
+            if(nums[right] < minK || nums[right] > maxK){
+                minI = right;
+                maxI = right;
+                left = right;
+            }
+            minI = nums[right] == minK ? right : minI;
+            maxI = nums[right] == maxK ?  right : maxI;
+            count += Math.min(minI, maxI) - left;
+            right++;
+        }
+        return count;
+    }
+}
+
+/* 
+class Solution {
+    public long countSubarrays(int[] nums, int minK, int maxK) {
+        long res = 0;
+        int badIdx = -1, leftIdx = -1, rightIdx = -1;
+
+        for (int i = 0; i < nums.length; ++i) {
+            if (!(minK <= nums[i] && nums[i] <= maxK)) {
+                badIdx = i;
+            }
+
+            if (nums[i] == minK) {
+                leftIdx = i;
+            }
+
+            if (nums[i] == maxK) {
+                rightIdx = i;
+            }
+
+            res += Math.max(0, Math.min(leftIdx, rightIdx) - badIdx);
+        }
+
+        return res;
+    }
+}
+*/
