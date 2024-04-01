7799d83567e7a0bf81f512edc5d4585957c14c6f
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-04-01 10:24:25 +0530

    Create Rotated_Array.cpp

---
+++ InterviewBit/Rotated_Array.cpp
@@ -0,0 +1,14 @@
+/* Link - https://www.interviewbit.com/problems/rotated-array/ */
+
+int Solution::findMin(const vector<int> &A) {
+    int low = 0, high = A.size()-1;
+    while(low <= high)
+    {
+        int mid = (low + high)/2;
+        if((mid == 0 || A[mid] < A[mid-1]) && (mid == A.size()-1 || A[mid] < A[mid+1]))
+            return A[mid];
+        else if(A[mid] > A[high])  low = mid+1;
+        else high = mid-1;
+    }
+    return -1; 
+}
