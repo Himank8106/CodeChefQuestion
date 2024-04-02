d5ebb640f835724f83d0452f6af8cb74f22da9a8
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-04-02 18:47:55 +0530

    Create Minimum_Absolute_Difference_In_BST.java

---
+++ GeeksforGeeks/Minimum_Absolute_Difference_In_BST.java
@@ -0,0 +1,25 @@
+/* Link - https://www.geeksforgeeks.org/problems/minimum-absolute-difference-in-bst-1665139652/1 */
+
+class Solution
+{
+    void inorder(Node root, List<Integer> list){
+        if(root == null) return;
+        inorder(root.left, list);
+        list.add(root.data);
+        inorder(root.right, list);
+    }
+    
+    int absolute_diff(Node root)
+    {
+        int diff=Integer.MAX_VALUE;
+        List<Integer> list = new ArrayList();       
+        inorder(root, list);       
+        
+        for(int i=1; i<list.size(); i++){
+            int abs_dif = list.get(i) - list.get(i-1);     
+            diff = Math.min(diff, abs_dif);       
+        }
+        
+        return diff;
+    }
+}
