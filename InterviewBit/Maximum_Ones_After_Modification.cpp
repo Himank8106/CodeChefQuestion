793d5489240a6d5d94ad8f0c90c79daa364ea6e9
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-04-12 20:22:38 +0530

    Create Maximum_Ones_After_Modification.cpp

---
+++ InterviewBit/Maximum_Ones_After_Modification.cpp
@@ -0,0 +1,15 @@
+/* Link - https://www.interviewbit.com/problems/maximum-ones-after-modification/ */
+
+int Solution::solve(vector<int> &A, int B) {
+    int n = A.size();
+    int l = 0, ctr = 0, Zct = 0;
+    for (int i = 0; i < n; i++) {
+        if (A[i] == 0) Zct++;
+        while (Zct > B) {
+            if (A[l] == 0) Zct--;
+            l++;
+        }
+        ctr = max(ctr, i - l + 1);
+    }
+    return ctr;
+}
