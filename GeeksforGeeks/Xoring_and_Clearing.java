09be4042d97df9c5d6845cf7341c7ea7c627277b
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-04-14 10:04:16 +0530

    Create Xoring_and_Clearing.java

---
+++ GeeksforGeeks/Xoring_and_Clearing.java
@@ -0,0 +1,23 @@
+/* Link - https://www.geeksforgeeks.org/problems/xoring-and-clearing/1 */
+
+class Solution {
+    public void printArr(int n, int arr[]) {
+        // code here
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public void setToZero(int n, int arr[]) {
+        // code here
+        Arrays.fill(arr, 0);
+    }
+
+    public void xor1ToN(int n, int arr[]) {
+        // code here
+        for(int i=0; i<n; i++){
+            arr[i] = i^arr[i];
+        }
+    }
+}
