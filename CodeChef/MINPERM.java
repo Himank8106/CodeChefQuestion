83e9bbb8a27f13b8a075e201e2331d939ab7b093
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-04-17 12:10:57 +0530

    Create MINPERM.java

---
+++ CodeChef/MINPERM.java
@@ -0,0 +1,44 @@
+/* Link - https://www.codechef.com/problems/MINPERM */
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		while(T-->0){
+		    int N=sc.nextInt();
+		    if(N%2==0){
+		        int start=2;
+		        for(int i=0;i<N;i=i+2){
+		            System.out.print(start+" ");
+		            //start--;
+		            System.out.print(start-1+" ");
+		            start+=2;
+		        }
+		        System.out.println();
+		    }else{
+		        int start=2;
+		        int run=N-3;
+		        for(int i=0;i<run;i=i+2){
+		            System.out.print(start+" ");
+		            //start--;
+		            System.out.print(start-1+" ");
+		            start+=2;
+		        }
+		        
+		        int sL=N-1;
+		        int fl=N;
+		        int l=N-2;
+		        System.out.println(sL+" "+fl+" "+l);
+		    }
+		}
+	}
+}
