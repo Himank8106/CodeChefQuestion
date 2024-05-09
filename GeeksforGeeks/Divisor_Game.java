cd782dab2949d2616c821d5a7ad9fb2e1798e526
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-05-09 20:57:45 +0530

    Create Divisor_Game.java

---
+++ GeeksforGeeks/Divisor_Game.java
@@ -0,0 +1,14 @@
+/* Link - https://www.geeksforgeeks.org/problems/divisor-game-1664432414/1 */
+
+class Solution {
+    public static boolean divisorGame(int n) {
+        // code here
+        if(n%2==0)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
