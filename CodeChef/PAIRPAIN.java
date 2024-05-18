23584aecb6ac85f0a385fed9c8dbbd079eb81388
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-05-18 15:17:10 +0530

    Create PAIRPAIN.java

---
+++ CodeChef/PAIRPAIN.java
@@ -0,0 +1,37 @@
+/* Link - https://www.codechef.com/problems/PAIRPAIN */
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner in = new Scanner(System.in);
+		int T = in.nextInt();
+		while(T-->0){
+		    int n = in.nextInt();
+		    long ans = 0, n2 = 0 , n1=0;
+		    
+		    for(int i=0;i<n;i++){
+		        int num = in.nextInt();
+		        if(num==1){
+		            n1++;
+		        }
+		        else if(num == 2)
+		            n2++;
+		    }
+		    
+		    ans += (n-n1)*n1;
+		    ans += (n1*(n1-1))/2;
+		    ans += (n2*(n2-1))/2;
+		    
+		    System.out.println(ans);
+		    
+		}
+	}
+}
