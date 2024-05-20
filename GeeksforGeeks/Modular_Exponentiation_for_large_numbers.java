b8c93c3163e916727140a824bb3ab3529e0b8475
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-05-20 23:47:23 +0530

    Create Modular_Exponentiation_for_large_numbers.java

---
+++ GeeksforGeeks/Modular_Exponentiation_for_large_numbers.java
@@ -0,0 +1,21 @@
+/* Link - https://www.geeksforgeeks.org/problems/modular-exponentiation-for-large-numbers5537/1 */
+
+class Solution
+{
+    public long PowMod(long x, long n, long m)
+    {
+        // Code here
+        long result=1;
+        if(m==1)
+        return 0;
+        x=x%m;
+        while(n>0){
+            if((n&1)==1){
+                result = (result*x)%m;
+            }
+            n=n/2;
+            x=(x*x)%m;
+        }
+        return result;
+    }
+}
