49368c0445fbea959061041e739c456b8d275cb7
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-07-01 23:24:08 +0530

    Create CHCHCL.java

---
+++ CodeChef/CHCHCL.java
@@ -0,0 +1,22 @@
+/* Link - https://www.codechef.com/problems/CHCHCL */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt(), m = sc.nextInt();
+            if(n%2!=0 && m%2!=0){
+               System.out.println("No");
+            }
+            else System.out.println("Yes");
+        }
+	}
+}
