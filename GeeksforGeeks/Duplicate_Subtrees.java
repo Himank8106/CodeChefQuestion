5d1b7986835e1ad641714ec71c06e299aa6dc9b0
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-07-04 13:07:38 +0530

    Create Duplicate_Subtrees.java

---
+++ GeeksforGeeks/Duplicate_Subtrees.java
@@ -0,0 +1,25 @@
+/* Link - https://www.geeksforgeeks.org/problems/duplicate-subtrees/1 */
+
+class Solution 
+{
+    Map<String, Integer> map;
+    public List<Node> printAllDups(Node root) 
+    {
+        ArrayList<Node> result = new ArrayList<>();
+        map = new HashMap<>();
+        inorder(root, result);
+        return result;
+    }
+    public String inorder(Node root,List<Node> result)
+    {
+        if(root==null)  return "";
+        String str = "(";
+        str += inorder(root.left, result);
+        str += Integer.toString(root.data);
+        str += inorder(root.right, result);
+        str += ")";
+        if(map.containsKey(str) && map.get(str)==1) result.add(root);
+        map.put(str, map.getOrDefault(str, 0)+1);
+        return str;
+    }
+}
