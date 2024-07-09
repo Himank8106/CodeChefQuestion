d1e7f4f0c67a7f26f944220a25d3fb4c3ca642a5
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-07-09 21:36:26 +0530

    Create Cousins_in_Binary_Tree.java

---
+++ InterviewBit/Cousins_in_Binary_Tree.java
@@ -0,0 +1,73 @@
+/* Link - https://www.interviewbit.com/problems/cousins-in-binary-tree/ */
+
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ArrayList<Integer> solve(TreeNode A, int B) {
+               Deque<TreeNode> q = new LinkedList<>();
+        q.add(A);
+
+        while(q.size()!=0)
+        {
+            int n = q.size();
+            boolean f1 = false;
+
+            for(int i=0; i<n; i++)
+            {
+
+                TreeNode ptr = q.pop();
+
+                if(ptr.left!=null)
+                {
+                    if(ptr.left.val == B)
+                    {
+                        f1 = true;
+                        continue;
+                    }
+                    else
+                    q.add(ptr.left);
+                }
+
+                if(ptr.right!=null)
+                {
+                    if(ptr.right.val!=B)
+                    q.add(ptr.right);
+                    else
+                    {
+                        if(q.peekLast()==ptr.left)
+                        q.pollLast();
+
+                        f1 = true;
+                    }
+                }
+            }
+
+            if(f1)
+            break;
+
+        }
+
+       
+
+        ArrayList<Integer> ans = new ArrayList<>();
+        while(q.size()!=0)
+        {
+            int v = q.pop().val;
+            ans.add(v);
+        }
+
+        return ans;
+
+    }
+}
