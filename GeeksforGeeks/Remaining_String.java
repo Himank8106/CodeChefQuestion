afe2c9f126a4cd03dd0416b32be2e191333a4f86
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-07-16 23:26:33 +0530

    Create Remaining_String.java

---
+++ GeeksforGeeks/Remaining_String.java
@@ -0,0 +1,12 @@
+/* Link - https://www.geeksforgeeks.org/problems/remaining-string3515/1 */
+
+class Solution {
+    public String printString(String s, char ch, int count) {
+        // code here
+        for(int i = 0;i<s.length();i++){
+            if(count == 0) return s.substring(i);
+            if(s.charAt(i) == ch) count--;
+        }
+        return "";
+    }
+}
