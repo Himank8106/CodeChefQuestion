0883c5efcad81921241973dc3ae806ce7e96c087
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-07-18 23:20:33 +0530

    Create Longest_alternating_subsequence.java

---
+++ GeeksforGeeks/Longest_alternating_subsequence.java
@@ -0,0 +1,43 @@
+/* Link - https://www.geeksforgeeks.org/problems/longest-alternating-subsequence5951/1 */
+
+class Solution {
+    public int alternatingMaxLength(int[] arr) {
+        // code 
+        Stack<Integer>stack=new Stack<>();
+        stack.push(arr[0]);
+        int n=arr.length;
+        int idx=1;
+        while(idx<n&&arr[idx]==arr[0]){
+            idx++;
+        }
+        boolean small=false;
+        if(idx<n&&arr[idx]<stack.peek()){
+            stack.push(arr[idx]);
+        }else{
+            small=true;
+            if(idx<n)
+            stack.push(arr[idx]);
+        }
+        idx++;
+        for(int i=idx;i<n;i++){
+            if(small==true){
+                if(arr[i]<stack.peek()){
+                    stack.push(arr[i]);
+                    small=false;
+                }else if(arr[i]>stack.peek()){
+                    stack.pop();
+                    stack.push(arr[i]);
+                }
+            }else{
+                if(arr[i]>stack.peek()){
+                    stack.push(arr[i]);
+                    small=true;;
+                }else if(arr[i]<stack.peek()){
+                    stack.pop();
+                    stack.push(arr[i]);
+                }
+            }
+        }
+        return stack.size();
+    }
+}
