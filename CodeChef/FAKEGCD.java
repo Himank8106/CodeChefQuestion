cc0f59885adb6c07ba79b901f6be69fe5878aaee
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-08-05 12:15:26 +0530

    Create FAKEGCD.java

---
+++ CodeChef/FAKEGCD.java
@@ -0,0 +1,22 @@
+/* Link - https://www.codechef.com/problems/FAKEGCD */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            for(int i=1; i<=n; i++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+	}
+}
