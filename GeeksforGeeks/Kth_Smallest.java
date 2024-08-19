e607cfb62170175938e434622ac2f0f83007bd87
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-08-19 22:58:10 +0530

    Create Kth_Smallest.java

---
+++ GeeksforGeeks/Kth_Smallest.java
@@ -0,0 +1,25 @@
+/* Link - https://www.geeksforgeeks.org/problems/kth-smallest-element5635/1 */
+
+class Solution {
+    public static int kthSmallest(int[] arr, int k) {
+        // Your code here
+        // Arrays.sort(arr);
+        // return arr[k-1];
+          int max = 1000000; // Given constraints: 1<=arr[i]<=10^6
+
+        int cnt[] = new int[max+1];  // Step 1
+
+        for(int num : arr) {   // Step 2
+            cnt[num]++;
+        }
+
+        int cumCount = 0;    
+        for(int i=1; i<=max; i++) {    // Step 3
+            cumCount += cnt[i];
+            if (cumCount >= k) {
+                return i;
+            }
+        }
+        return -1; // Just in case things go south
+    }
+}
