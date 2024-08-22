6354332142d910d95b31ff2a214d7cc53356b58d
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-08-22 12:10:02 +0530

    Create LIKECS01.java

---
+++ CodeChef/LIKECS01.java
@@ -0,0 +1,29 @@
+/* Link - https://www.codechef.com/problems/LIKECS01 */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            String s = sc.next();
+            HashSet<Character> hs = new HashSet<>();
+            boolean b = false;
+            for(int i=0; i<s.length(); i++){
+                if(hs.contains(s.charAt(i))){
+                    b = true;
+                    break;
+                }
+                else hs.add(s.charAt(i));
+            }
+            if(b) System.out.println("yes");
+            else System.out.println("no");
+        }
+	}
+}
