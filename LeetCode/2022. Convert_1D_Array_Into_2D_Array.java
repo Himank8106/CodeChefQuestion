39d2114875468d5dd4eef2a9a38f4fd451deeceb
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-09-01 19:50:13 +0530

    Create 2022. Convert_1D_Array_Into_2D_Array.java

---
+++ LeetCode/2022. Convert_1D_Array_Into_2D_Array.java
@@ -0,0 +1,16 @@
+/* Link - https://leetcode.com/problems/convert-1d-array-into-2d-array/ */
+
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        if (m * n != original.length) {
+            return new int[0][0]; 
+        }
+        int[][] arr2D = new int[m][n];
+        for (int i = 0; i < m; i++) {       
+            for (int j = 0; j < n; j++) {    
+                arr2D[i][j] = original[i * n + j];
+            }
+        }
+        return arr2D;
+    }
+}
