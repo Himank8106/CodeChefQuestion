9a4014348246ae1d10f6aa57d253307927113ce8
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-09-02 21:26:51 +0530

    Create 1894. Find_the_Student_that_Will_Replace_the Chalk.java

---
+++ LeetCode/1894. Find_the_Student_that_Will_Replace_the Chalk.java
@@ -0,0 +1,21 @@
+/* Link - https://leetcode.com/problems/find-the-student-that-will-replace-the-chalk/ */
+
+class Solution {
+    public int chalkReplacer(int[] chalk, int initialChalkPieces) {
+        long totalChalkNeeded = 0;
+        for (int studentChalkUse : chalk) {
+            totalChalkNeeded += studentChalkUse;
+        }
+        
+        int remainingChalk = (int)(initialChalkPieces % totalChalkNeeded);
+        
+        for (int studentIndex = 0; studentIndex < chalk.length; studentIndex++) {
+            if (remainingChalk < chalk[studentIndex]) {
+                return studentIndex;
+            }
+            remainingChalk -= chalk[studentIndex];
+        }
+        
+        return 0;  
+    }
+}
