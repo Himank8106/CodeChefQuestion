9d9d0fb4058a4f8fb2b37c4365b5105e6a0e434e
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-10-15 15:20:20 +0530

    Create 2938. Separate_Black_and_White_Balls.java

---
+++ LeetCode/2938. Separate_Black_and_White_Balls.java
@@ -0,0 +1,13 @@
+/* Link - https://leetcode.com/problems/separate-black-and-white-balls/ */
+
+class Solution {
+    public long minimumSteps(String s) {
+        long ans = 0;
+        int black = 0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='0') ans += black;
+            else black++;
+        }
+        return ans;
+    }
+}
