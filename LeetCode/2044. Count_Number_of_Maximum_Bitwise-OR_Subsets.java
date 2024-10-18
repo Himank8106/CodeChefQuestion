09977b601655ae392829e6dadc0741bbbb0531f2
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-10-18 18:35:51 +0530

    Create 2044. Count_Number_of_Maximum_Bitwise-OR_Subsets.java

---
+++ LeetCode/2044. Count_Number_of_Maximum_Bitwise-OR_Subsets.java
@@ -0,0 +1,18 @@
+/* Link - https://leetcode.com/problems/count-number-of-maximum-bitwise-or-subsets/ */
+
+class Solution {
+    public void backtrack(int[] nums, int index, int currentOR, int maxOR, int[] count) {
+        if (currentOR == maxOR) count[0]++;
+        for (int i = index; i < nums.length; i++) {
+            backtrack(nums, i + 1, currentOR | nums[i], maxOR, count);
+        }
+    }
+
+    public int countMaxOrSubsets(int[] nums) {
+        int maxOR = 0;
+        for (int num : nums) maxOR |= num;
+        int[] count = new int[1];
+        backtrack(nums, 0, 0, maxOR, count);
+        return count[0];
+    }
+}
