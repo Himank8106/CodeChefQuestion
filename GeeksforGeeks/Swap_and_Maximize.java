b236b798944864916c4a50a4e8e6e47cf9a2237b
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-11-01 22:16:57 +0530

    Create Swap_and_Maximize.java

---
+++ GeeksforGeeks/Swap_and_Maximize.java
@@ -0,0 +1,16 @@
+/* Link - https://www.geeksforgeeks.org/problems/swap-and-maximize5859/1 */
+
+class Solution {
+    public long maxSum(Long[] arr) {
+        // code here
+        Arrays.sort(arr);
+        long sum = 0;
+        int i = 0, j = arr.length-1;
+        while(i<j){
+            sum+= (Math.abs(arr[j]-arr[i++]) + Math.abs(arr[j--]-arr[i]));
+            
+        }
+        sum+= Math.abs(arr[i]-arr[0]);
+        return sum;
+    }
+}
