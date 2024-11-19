878f9246410a85122a6df09c3d85a0eedfb02821
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-11-19 10:14:58 +0530

    Update 2461. Maximum_Sum_of_Distinct_Subarrays_With_Length_K.java

--- LeetCode/2461. Maximum_Sum_of_Distinct_Subarrays_With_Length_K.java
+++ LeetCode/2461. Maximum_Sum_of_Distinct_Subarrays_With_Length_K.java
@@ -2,29 +2,29 @@
 
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-        int n = nums.length;
-        Set<Integer> elements = new HashSet<>();
-        int maxSum = 0, currentSum = 0, begin = 0;
-        for(int end = 0; end<n; end++){
-            if(!elements.contains(nums[end])){
-                currentSum += nums[end];
-                elements.add(nums[end]);
-                if (end - begin + 1 == k) {
-                    maxSum = Math.max(maxSum, currentSum);
-                    currentSum -= nums[begin];
-                    elements.remove(nums[begin]);
-                    begin++;
+        long res = 0;
+        Map<Integer, Integer> count = new HashMap<>();
+        long curSum = 0;
+        
+        int l = 0;
+        for (int r = 0; r < nums.length; r++) {
+            curSum += nums[r];
+            count.put(nums[r], count.getOrDefault(nums[r], 0) + 1);
+            
+            if (r - l + 1 > k) {
+                count.put(nums[l], count.get(nums[l]) - 1);
+                if (count.get(nums[l]) == 0) {
+                    count.remove(nums[l]);
                 }
-            } else {
-                while (nums[begin] != nums[end]) {
-                    currentSum -= nums[begin];
-                    elements.remove(nums[begin]);
-                    begin++;
-                }
-                begin++;
+                curSum -= nums[l];
+                l++;
+            }
+            
+            if (count.size() == r - l + 1 && r - l + 1 == k) {
+                res = Math.max(res, curSum);
             }
         }
         
-        return maxSum;
+        return res;
     }
 }
