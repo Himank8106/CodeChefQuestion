b035a1b35ada8d388985946e800e0b2cbb27149a
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-11-20 11:24:14 +0530

    Create Majority_Element_II.java

---
+++ GeeksforGeeks/Majority_Element_II.java
@@ -0,0 +1,25 @@
+/* Link - https://www.geeksforgeeks.org/problems/majority-vote/1 */
+
+class Solution {
+    // Function to find the majority elements in the array
+    public List<Integer> findMajority(int[] nums) {
+       List<Integer> list = new ArrayList<>();
+       HashMap<Integer,Integer> map = new HashMap<>();
+       HashSet<Integer> set= new HashSet<>();
+       int n= nums.length;
+       int m = n/3;
+       for(int i=0;i<n;i++)
+       {
+           map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+           set.add(nums[i]);
+       }
+       for(Integer i: set)
+       {
+           if(map.containsKey(i) && map.get(i)>m)
+           {
+               list.add(i);
+           }
+       }
+       return list;
+    }
+}
