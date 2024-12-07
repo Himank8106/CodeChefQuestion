fc5fba799d606929afe9bc3e9f0153b124103011
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-12-07 23:23:33 +0530

    Create 1760. Minimum_Limit_of_Balls_in_a_Bag.java

---
+++ LeetCode/1760. Minimum_Limit_of_Balls_in_a_Bag.java
@@ -0,0 +1,17 @@
+/* Link - https://leetcode.com/problems/minimum-limit-of-balls-in-a-bag/ */
+
+class Solution {
+    public int minimumSize(int[] nums, int maxOps) {
+        int low = 1, high = Arrays.stream(nums).max().getAsInt();
+        while (low < high) {
+            int mid = (low + high) / 2;
+            int ops = 0;
+            for (int n : nums) {
+                ops += (n - 1) / mid;
+            }
+            if (ops <= maxOps) high = mid;
+            else low = mid + 1;
+        }
+        return high;
+    }
+}
