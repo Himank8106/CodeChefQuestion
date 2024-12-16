c3b7f75d5efe60c65c36346f9f153cffd882f0d2
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-12-16 22:05:48 +0530

    Create 3264. Final_Array_State_After_K_Multiplication_Operations_I.java

---
+++ LeetCode/3264. Final_Array_State_After_K_Multiplication_Operations_I.java
@@ -0,0 +1,28 @@
+/* Link - https://leetcode.com/problems/final-array-state-after-k-multiplication-operations-i/ */
+
+class Solution {
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+        int[] arr = new int[2];
+
+        for (int i = 0; i < k; i++) {
+            arr = getMin(nums);
+            nums[arr[1]] = arr[0] * multiplier;
+        }
+
+        return nums;
+    }
+
+    private int[] getMin(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int idx = 0;
+
+        for (int i = 0; i < nums.length; i++)
+            if (nums[i] < min) {
+                min = nums[i];
+                idx = i;
+            }
+
+        return new int[] { min, idx };
+    }
+
+}
