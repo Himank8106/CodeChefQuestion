141c0b9571043330ba823a801d22446ef7072fbc
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2024-12-27 22:07:19 +0530

    Create 1014. Best_Sightseeing_Pair.java

---
+++ LeetCode/1014. Best_Sightseeing_Pair.java
@@ -0,0 +1,12 @@
+/* Link - https://leetcode.com/problems/best-sightseeing-pair/ */
+
+class Solution {
+    public int maxScoreSightseeingPair(int[] values) {
+        int ans = Integer.MIN_VALUE, m = values[0];
+        for(int i=1; i<values.length; i++){
+            if(values[i]-i+m>ans) ans = values[i]-i+m;
+            if(values[i]+i>m) m = values[i]+i;
+        }
+        return ans;
+    }
+}
