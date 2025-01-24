da4896ba02916c7f4a31ae24aab11c90ee32f9f0
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2025-01-24 21:50:06 +0530

    Create Detect_Loop_in_linked_list.java

---
+++ GeeksforGeeks/Detect_Loop_in_linked_list.java
@@ -0,0 +1,26 @@
+/* Link - https://www.geeksforgeeks.org/problems/detect-loop-in-linked-list/1 */
+
+/* Node is defined as
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {data = d; next = null; }
+}
+
+*/
+
+class Solution {
+    // Function to check if the linked list has a loop.
+    public static boolean detectLoop(Node head) {
+        // Add code here
+        Node slow = head, fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast) return true;
+        }
+        return false;
+    }
+}
