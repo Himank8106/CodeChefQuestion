9bbed31193863544550ff88d0286d66563398c33
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2025-01-29 22:09:53 +0530

    Create Implement_Pow.java

---
+++ GeeksforGeeks/Implement_Pow.java
@@ -0,0 +1,27 @@
+/* Link - https://www.geeksforgeeks.org/problems/powx-n/1 */
+
+class Solution {
+    double power(double b, int e) {
+        // code here
+        if(e<0){
+            return 1/power(b,-e);
+        }
+        if(e==0){
+            if(b<0 || b>0){
+                return 1.0;
+            }
+            else if(b==0){
+                return 0.0;
+            }
+        }
+        
+        double ans = power(b, e/2);
+        
+        if(e%2==0){
+            return ans*ans;
+        }
+        else {
+            return b*ans*ans;
+        }
+    }
+}
