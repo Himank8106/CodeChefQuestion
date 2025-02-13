f52e6746323e936bd646cb2a9cf92ed6a923f1fe
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2025-02-13 21:48:21 +0530

    Create Pair_Sum_in_BST.java

---
+++ GeeksforGeeks/Pair_Sum_in_BST.java
@@ -0,0 +1,25 @@
+/* Link - https://www.geeksforgeeks.org/problems/find-a-pair-with-given-target-in-bst/1 */
+
+/*
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int d) {
+        data = d;
+        left = right = null;
+    }
+}
+*/
+class Solution {
+    HashSet<Integer> s=new HashSet<>();
+    boolean findTarget(Node root, int target) {
+        if(root==null) return false;
+        boolean l=findTarget(root.left,target);
+        boolean temp=false;
+        if(s.contains(target-root.data)) temp=true;
+        s.add(root.data);
+        boolean r=findTarget(root.right,target);
+        return (l||r)||temp;
+    }
+}
