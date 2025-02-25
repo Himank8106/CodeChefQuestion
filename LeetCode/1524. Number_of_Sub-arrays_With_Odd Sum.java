9272de51e5ec1a2c69e6b07d6a94b67a61e14e28
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2025-02-25 19:24:59 +0530

    Create 1524. Number_of_Sub-arrays_With_Odd Sum.java

---
+++ LeetCode/1524. Number_of_Sub-arrays_With_Odd Sum.java
@@ -0,0 +1,13 @@
+/* Link - https://leetcode.com/problems/number-of-sub-arrays-with-odd-sum/ */
+
+class Solution {
+    public int numOfSubarrays(int[] arr) {
+        long oddCount = 0, prefixSum = 0;
+        for(int a : arr){
+            prefixSum += a;
+            oddCount += prefixSum % 2;
+        }
+        oddCount += (arr.length - oddCount) * oddCount;
+        return (int)(oddCount % 1_000_000_007);
+    }
+}
