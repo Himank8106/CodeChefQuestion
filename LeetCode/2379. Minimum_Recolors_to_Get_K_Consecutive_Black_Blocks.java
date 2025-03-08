b7e31ad315f5ea1033d400ac6c6d7c2649e91bfb
Himank Joshi <101801844+Himank8106@users.noreply.github.com>
2025-03-08 22:18:16 +0530

    Create 2379. Minimum_Recolors_to_Get_K_Consecutive_Black_Blocks.java

---
+++ LeetCode/2379. Minimum_Recolors_to_Get_K_Consecutive_Black_Blocks.java
@@ -0,0 +1,15 @@
+/* Link - https://leetcode.com/problems/minimum-recolors-to-get-k-consecutive-black-blocks/ */
+
+class Solution {
+    public int minimumRecolors(String blocks, int k) {
+        int blackCount = 0, ans = Integer.MAX_VALUE;
+    
+        for (int i = 0; i < blocks.length(); i++) {
+            if (i - k >= 0 && blocks.charAt(i - k) == 'B') blackCount--;
+            if (blocks.charAt(i) == 'B') blackCount++;
+            ans = Math.min(ans, k - blackCount);
+        }
+        
+        return ans;
+    }
+}
